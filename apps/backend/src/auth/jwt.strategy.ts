ed34af343f6c2206eaa67ecf8608cc0586da409d
Shray Jain <76147471+shray-jayn@users.noreply.github.com>
2025-01-31 16:21:25 +0530

    Added Auth Module

---
+++ apps/backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly prisma: PrismaService, 
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    const { sub: userId } = payload;
+
+    const user = await this.prisma.userData.findUnique({
+      where: { userId: userId }, 
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid token. Please log in again.');
+    }
+
+    return user;
+  }
+}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,47 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import * as morgan from 'morgan';
 
-async function bootstrap() {
+dotenv.config();
+
+async function bootstrap() {``
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const corsOrigins = process.env.CORS_ORIGIN
+    ? process.env.CORS_ORIGIN.split(',')
+    : ['http://localhost:5173'];
+
+  app.enableCors({
+    origin: function (origin, callback) {
+      console.log(`CORS request from origin: ${origin}`);
+      if (!origin || corsOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.error(`Blocked by CORS: ${origin}`);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.use(morgan('dev'));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`🚀 Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
